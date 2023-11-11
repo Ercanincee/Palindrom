a4603d420d7c6229c04e4fc34147d9cf1f80d17a
Ercanincee <132362073+Ercanincee@users.noreply.github.com>
2023-11-11 14:40:40 +0300

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("polindrom sayiyi bulma uygulamasina hosgeldiniz");
+string girilendeger = Console.ReadLine();
+int duz = 0;
+int ters = girilendeger.Length - 1;
+while (duz < ters)
+{
+    if (girilendeger[duz] == girilendeger[ters]) 
+    {   duz++;
+        ters--;
+    }
+    else {
+        Console.WriteLine("Girdiginiz Deger Polindrom Degildir");
+        break; }
+    Console.WriteLine("Evet Polindrom bir deger Girdiniz");
+}
+
